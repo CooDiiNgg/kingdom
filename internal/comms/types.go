52f43da2ce80a2aa71facab3bb5a9de88bf4265f
CooDiiNgg <93319862+CooDiiNgg@users.noreply.github.com>
2025-05-01 13:44:03 +0300

    NEW: Added the base types that will be used for the protocol

---
+++ internal/comms/types.go
@@ -0,0 +1,23 @@
+package comms
+
+type Request struct {
+	AgentID  string `json:"agent_id"`
+	Hostname string `json:"hostname"`
+	OS       string `json:"os"`
+	IPAddr   string `json:"ipaddr"`
+	Port     int    `json:"port"`
+}
+
+type Task struct {
+	ID      string `json:"id"`
+	Command string `json:"command"`
+	Args    string `json:"args"`
+}
+
+type TaskResult struct {
+	AgentID string `json:"agent_id"`
+	TaskID  string `json:"task_id"`
+	Status  string `json:"status"`
+	Output  string `json:"output"`
+	Error   string `json:"error"`
+}
