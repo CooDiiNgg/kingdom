258421d186d56a71c7e1b3a5491ea39ef9e45235
CooDiiNgg <93319862+CooDiiNgg@users.noreply.github.com>
2025-05-01 23:24:17 +0300

    UPDATE: Made new exposed encoders/decoders that use the encryption/decryption for the protocol

--- internal/comms/format.go
+++ internal/comms/format.go
@@ -4,12 +4,40 @@ import (
 	"encoding/json"
 )
 
-func Encode(v any) ([]byte, error) {
+func encode_internal(v any) ([]byte, error) {
 	return json.Marshal(v)
 }
 
-func Decode[T any](data []byte) (T, error) {
+func decode_internal[T any](data []byte) (T, error) {
 	var result T
 	err := json.Unmarshal(data, &result)
 	return result, err
+}
+
+func Encode(v any) ([]byte, []byte, error) {
+	key, err := generateKey()
+	if err != nil {
+		return nil, nil, err
+	}
+	encodedData, err := encode_internal(v)
+	if err != nil {
+		return nil, nil, err
+	}
+	encryptedData, err := encrypt(encodedData, key)
+	if err != nil {
+		return nil, nil, err
+	}
+	return encryptedData, key, nil
+}
+
+func Decode[T any](data []byte, key []byte) (T, error) {
+	decodedData, err := decrypt(data, key)
+	if err != nil {
+		return *new(T), err
+	}
+	result, err := decode_internal[T](decodedData)
+	if err != nil {
+		return *new(T), err
+	}
+	return result, nil
 }
