7ac138c294724f6ae06e379d60c2d24ae04613b0
CooDiiNgg <93319862+CooDiiNgg@users.noreply.github.com>
2025-05-02 16:15:41 +0300

    UPDATE: made all test cases for now

--- test/unit/comms_cases.go
+++ test/unit/comms_cases.go
@@ -177,3 +177,153 @@ var CommsTestCases_Encrypt = []CommsTestCase_Encrypt{
 		Error:    true,
 	},
 }
+
+var CommsTestCases_Decrypt = []CommsTestCase_Decrypt{
+	{
+		Name:     "Happy path 1",
+		Input:    []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("12345678901234567890123456789012"),
+		Expected: []byte("Hello, World!"),
+		Error:    false,
+	},
+	{
+		Name:     "Happy path 2",
+		Input:    []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("12345678901234567890123456789012"),
+		Expected: []byte(`{"agent_id":"agent1","hostname":"host1","os":"linux"}`),
+		Error:    false,
+	},
+	{
+		Name:     "Negative path 1",
+		Input:    []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("1234567890123456789012345678901"),                                                              // 31 bytes
+		Expected: nil,
+		Error:    true,
+	},
+	{
+		Name:     "Negative path 2",
+		Input:    []byte("Hello, World!"),
+		Key:      []byte("12345678901234567890123456789012"),
+		Expected: nil,
+		Error:    true,
+	},
+}
+
+var CommsTestCases_Encode = []CommsTestCase_Encode{
+	{
+		Name: "Happy path 1",
+		Input: &comms.Request{
+			AgentID:  "agent1",
+			Hostname: "host1",
+			OS:       "linux",
+			IPAddr:   "127.0.0.1",
+			Port:     8080,
+		},
+		Expected: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("12345678901234567890123456789012"),
+		Error:    false,
+	},
+	{
+		Name: "Happy path 2",
+		Input: &comms.Task{
+			ID:      "task1",
+			Command: "ls",
+			Args:    "-l",
+		},
+		Expected: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("12345678901234567890123456789012"),
+		Error:    false,
+	},
+	{
+		Name: "Happy path 3",
+		Input: &comms.TaskResult{
+			AgentID: "agent1",
+			TaskID:  "task1",
+			Status:  "success",
+			Output:  "output",
+			Error:   "",
+		},
+		Expected: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("12345678901234567890123456789012"),
+		Error:    false,
+	},
+	{
+		Name:     "Negative path 1",
+		Input:    make(chan int),
+		Expected: nil,
+		Key:      []byte("12345678901234567890123456789012"),
+		Error:    true,
+	},
+	{
+		Name: "Negative path 2",
+		Input: &comms.Task{
+			ID:      "task2",
+			Command: "ls",
+			Args:    "-l",
+		},
+		Expected: nil,
+		Key:      []byte("1234567890123456789012345678901"), // 31 bytes
+		Error:    true,
+	},
+}
+
+var CommsTestCases_Decode = []CommsTestCase_Decode{
+	{
+		Name:  "Happy path 1",
+		Input: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:   []byte("12345678901234567890123456789012"),
+		Expected: &comms.Request{
+			AgentID:  "agent1",
+			Hostname: "host1",
+			OS:       "linux",
+			IPAddr:   "127.0.0.1",
+			Port:     8080,
+		},
+		Error: false,
+	},
+	{
+		Name:  "Happy path 2",
+		Input: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:   []byte("12345678901234567890123456789012"),
+		Expected: &comms.Task{
+			ID:      "task1",
+			Command: "ls",
+			Args:    "-l",
+		},
+		Error: false,
+	},
+	{
+		Name:  "Happy path 3",
+		Input: []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:   []byte("12345678901234567890123456789012"),
+		Expected: &comms.TaskResult{
+			AgentID: "agent1",
+			TaskID:  "task1",
+			Status:  "success",
+			Output:  "output",
+			Error:   "",
+		},
+		Error: false,
+	},
+	{
+		Name:     "Negative path 1",
+		Input:    []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values
+		Key:      []byte("1234567890123456789012345678901"),                                                              // 31 bytes
+		Expected: nil,
+		Error:    true,
+	},
+	{
+		Name:     "Negative path 2",
+		Input:    []byte("Hello, World!"),
+		Key:      []byte("12345678901234567890123456789012"),
+		Expected: nil,
+		Error:    true,
+	},
+	{
+		Name:     "Negative path 3",
+		Input:    []byte{0x8a, 0x1b, 0x2c, 0x3d, 0x4e, 0x5f, 0x6a, 0x7b, 0x8c, 0x9d, 0xae, 0xbf, 0xd0, 0xe1, 0xf2, 0x03}, // Will change later to real values - they should decrypt to a string
+		Key:      []byte("12345678901234567890123456789012"),
+		Expected: nil,
+		Error:    true,
+	},
+}
