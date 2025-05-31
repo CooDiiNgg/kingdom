5eaa73c5244a15cdbea3e9a9cfc83b9b0083ed26
Nikolay Valkanov <niki@valkanovi.com>
2025-05-31 18:24:56 +0300

    UPDATE: some internal logic of the http - again probably will change later

--- internal/listeners/http.go
+++ internal/listeners/http.go
@@ -1,2 +1,63 @@
 package listeners
 
+import (
+	"net/http"
+)
+
+type HTTPListener struct {
+	address string
+	port    int
+	clientID string
+	server *http.Server
+}
+
+func (h *HTTPListener) Start(clientID string) error {
+	h.clientID = clientID
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(fmt.Sprintf("/client/%s", h.clientID), func(w http.ResponseWriter, r *http.Request) {
+		resp, err := HandleRequest(h.clientID, r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(resp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	if h.address == "" {
+		h.address = "localhost"
+	}
+	if h.port == 0 {
+		h.port = 8080
+	}
+
+	h.server = &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", h.address, h.port),
+		Handler: mux,
+	}
+
+	go h.server.ListenAndServe()
+	return nil
+}
+
+func (h *HTTPListener) Stop() error {
+	if h.server != nil {
+		return h.server.Close()
+	}
+	return nil
+}
+
+func (h *HTTPListener) Configure(addr ListenerAddr) error {
+	if addr.Address != "" {
+		h.address = addr.Address
+	}
+	if addr.Port != 0 {
+		h.port = addr.Port
+	}
+	return nil
+}
