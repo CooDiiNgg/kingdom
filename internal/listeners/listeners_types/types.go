00ce4ee2ad89c1dc12e0ccd64b60790514fb4c9e
Nikolay Valkanov <niki@valkanovi.com>
2025-05-31 18:23:44 +0300

    UPDATE: Added options for external hosting of listeners - not really useful though

--- internal/listeners/listeners_types/types.go
+++ internal/listeners/listeners_types/types.go
@@ -3,4 +3,10 @@ package listenerstypes
 type Listener interface {
 	Start(clientID string) error
 	Stop() error
+	Configure(addr ListenerAddr) error
+}
+
+type ListenerAddr struct {
+	Address string `json:"address"`
+	Port    int    `json:"port"`
 }
